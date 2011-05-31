b6a7db6a884708132a20cbd5d7cfc88b9cc9cde2
Matt Brown <gowabash@gmail.com>
2011-05-31 08:39:35 -0400

    Initial import

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "http://rubygems.org"
+gem 'bundler'
+gem 'sinatra'
+gem 'mongo'
+gem 'bson'
+gem 'bson_ext'
+gem 'json'
---
+++ hello.rb
@@ -0,0 +1,53 @@
+require 'sinatra'
+require 'mongo'
+require 'yaml'
+require 'json'
+
+RAILS_ENV=ENV['RACK_ENV']
+
+def get_connection()
+  options = {}
+  options['server'] = JSON.parse( ENV['VCAP_SERVICES'] )['mongodb-1.8'].first['credentials']['hostname'] rescue 'localhost'
+  options['port'] = JSON.parse( ENV['VCAP_SERVICES'] )['mongodb-1.8'].first['credentials']['port'] rescue 27017
+  options['database'] = JSON.parse( ENV['VCAP_SERVICES'] )['mongodb-1.8'].first['credentials']['db'] rescue 'gowabash'
+  options['username'] = JSON.parse( ENV['VCAP_SERVICES'] )['mongodb-1.8'].first['credentials']['username'] rescue ''
+  options['password'] = JSON.parse( ENV['VCAP_SERVICES'] )['mongodb-1.8'].first['credentials']['password'] rescue ''
+  return options
+end
+
+get '/' do
+  vars = ""
+  ENV.each_key do |key|
+    vars += "#{key} is #{ENV[key]}<br>"
+  end
+  options = get_connection
+  options.each_key do |key|
+    vars += "#{key} is #{options[key]}<br>"
+  end
+  
+  return vars
+end
+
+get '/add' do
+  options = get_connection
+  conn= Mongo::Connection.new(options['server'], options['port'], options) 
+  db = conn.db(options['database'])
+  item = db['counters'].update({'name' => 'test'}, {'$inc' => {'value' => 1}}, {:upsert => true, :safe => true})
+  return "added it #{item.inspect}"
+end
+
+get '/start' do
+  options = get_connection
+  conn= Mongo::Connection.new(options['server'], options['port'], options) 
+  db = conn.db(options['database'])
+  item = db['counters'].insert({'name' => 'test', 'value' => 1})
+  return "added it #{item.inspect}"
+end
+
+get '/list' do
+  options = get_connection
+  conn= Mongo::Connection.new(options['server'], options['port'], options) 
+  db = conn.db(options['database'])
+  counter = db['counters'].find({'name' => 'test'}).to_a
+  return "the counter is #{counter.inspect}"
+end
